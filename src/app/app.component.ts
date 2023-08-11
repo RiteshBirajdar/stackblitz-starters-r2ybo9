d79fdc7e3b469012f5c72576e5a51cf9fd9f8933
RiteshBirajdar <105624241+RiteshBirajdar@users.noreply.github.com>
2023-08-11 15:14:23 +0530

    3 files modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HerosdetailComponent } from './herosdetail/herosdetail.component';
 import { HEROES } from './mock-heroes';
 @Component({
   standalone: true,
   selector: 'app-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports:[HeroesComponent,CommonModule],
+  imports:[HeroesComponent,CommonModule,HerosdetailComponent],
 })
 export class AppComponent implements OnInit {
 
---
+++ src/app/herosdetail/herosdetail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone:true,
+  selector: 'app-herosdetail',
+  templateUrl: './herosdetail.component.html',
+  styleUrls: ['./herosdetail.component.css']
+})
+export class HerosdetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
