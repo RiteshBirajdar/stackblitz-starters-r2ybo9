d06fc56915a5d0618d700a4d93d57659758f4350
RiteshBirajdar <105624241+RiteshBirajdar@users.noreply.github.com>
2023-08-11 10:16:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-r2ybo9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-r2ybo9)
---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { HerosComponent } from './heros/heros.component';
+
+@Component({
+  standalone: true,
+  selector: 'app-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports:[HerosComponent],
+})
+export class AppComponent implements OnInit {
+
+  title = 'Tour of Heroes';
+  constructor() {}
+
+  ngOnInit() {}
+}
